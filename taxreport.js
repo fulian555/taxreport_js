750022009b43766440dc90ae6e13d093b9f31d94
fulian555 <fulian555@gmail.com>
2015-03-29 22:52:56 -0400

    support multi input type and text input type

--- taxreport.js
+++ taxreport.js
@@ -3,34 +3,84 @@ var g_tpCurFormNo = 0;
 var g_tpForm = [
 	//question 1
 	{//self
-		attr: "self",
+		name: "self",
 		tips: "您是否为自己报税?",
 		inputs: {
 			type: "radio",
 			value: ["是","不是"],
-			next: [null,null],
-			defaultchecked: 0,				
+			next: [null,null]			
+		},
+		prev: null
+	},
+	{//personel info
+		name: "ps",
+		tips: "请输入报税人的个人信息",
+		inputs: {
+			type: "multi",
+			subinputs: [
+				{
+					name: "sex",
+					tips: "性别：",
+					inputs: {
+						type: "radio",
+						value: ["女", "男"],
+						next: [null, null]
+					},
+					prev: null
+				},
+				{
+					name: "lastname",
+					tips: "姓：",
+					inputs: {
+						type: "text",
+						attr: {
+							name: [],
+							value:[]
+						},
+						validate: {
+							required: true,
+							regularE: "[a-zA-Z]"
+						},
+					},
+					prev: null
+				},
+				{
+					name: "firstname",
+					tips: "名：",
+					inputs: {
+						type: "text",
+						attr: {
+							name: [],
+							value:[]
+						},
+						validate: {
+							required: true,
+							regularE: "[a-zA-Z]"
+						},
+					},
+					prev: null
+				}				
+			],			
 		},
 		prev: null,
 	},
 	{//marriage
-		attr: "marriage",
+		name: "marriage",
 		tips: "请选择您的婚姻状况?",
 		inputs:{
 			type: "radio",
 			value: ["未婚","已婚","同居"],
-			next: [null,null,null],
-			defaultchecked: 0,
+			next: [null,null,null]
 		},
-		prev: null,
+		prev: null
 	},
 	{
-		attr: "end",
+		name: "end",
 		tips: "信息搜集完成",
 		prev: null,
 		inputs : {
 			type: "none",
-		},
+		}
 	},
 ];
 
@@ -39,39 +89,89 @@ var classTips = "tp-tips";
 function idDiv(formNumber) {
 	return "tp"+formNumber;
 }
-function idTips(attr) {
-	return "tp-tips-"+attr;
+function idTips(name) {
+	return "tp-tips-"+name;
 }
-function nameRadio(attr) {
-	return "tp_"+attr;
+function nameAttr(name) {
+	return "tp_"+name;
+}
+
+function radioInput(name, radioObj) {
+	var radiocode="";
+	
+	for (var j = 0; j < radioObj.value.length; j++) {
+		//add the radio
+		radiocode = radiocode + '<input type="radio" name="'+nameAttr(name)+'"' + 'value="' + j + '"';
+		if (j == 0) {
+			radiocode = radiocode + ' checked="checked"/>';
+		} else {
+			radiocode = radiocode + '/>';
+		}
+		//add the label
+		radiocode = radiocode + '<label for="tp-rd-' + name + '">' + radioObj.value[j] + '</label>';
+	}	
+	
+	return radiocode;
+}
+
+function textInput(name, textObj) {
+	var textcode = "";
+	
+	textcode = textcode + '<input type="text" name="' + nameAttr(name) + '"';	
+	
+	textcode = textcode + '/>';
+	return textcode;
+}
+
+function multiInput(name, multiObj) {
+	var inputsNum = multiObj.subinputs.length;
+	var multicode = "";
+	var tmpForm;
+	
+	for (var i = 0; i < inputsNum; i++) {
+		tmpForm = multiObj.subinputs[i];
+		//add the tips
+		multicode = multicode + '<p class="'+classTips+'" id="'+idTips(tmpForm.name) + '">'+tmpForm.tips+'</p>';
+		//process the subinput fild
+		switch (tmpForm.inputs.type) {
+			case "radio" :
+				multicode = multicode + radioInput(tmpForm.name, tmpForm.inputs );			
+				break;
+			case "text":
+				multicode = multicode + textInput(tmpForm.name, tmpForm.inputs );
+				break;
+			case "multi" :
+				multicode = multicode + multiInput(tmpForm.name, tmpForm.inputs );	
+				break;
+				
+			default:
+				break;
+		}
+	}
+	multicode = multicode + '<br />';
+	return multicode;
 }
 
 function formgenerate(formNum) {
 	var newForm = g_tpForm[formNum];
 	
 	var fdiv = document.createElement("div");
-	//fdiv.setAttribute('id',"tp"+formNum);
+	//fdiv.setnameibute('id',"tp"+formNum);
 	fdiv.setAttribute('id',idDiv(formNum));
 	fdiv.className = classDiv;
-	
 	//add the tips
-	var htmlcode = '<p class="'+classTips+'" id="'+idTips(newForm.attr) + '">'+newForm.tips+'</p>';
-		
+	var htmlcode = '<p class="'+classTips+'" id="'+idTips(newForm.name) + '">'+newForm.tips+'</p>';
+	
 	//process the input fild
 	switch (newForm.inputs.type) {
 		case "radio" :
-			for (var j = 0; j < newForm.inputs.value.length; j++) {
-				//add the radio
-				htmlcode = htmlcode + '<input type="radio" name="'+nameRadio(newForm.attr)+'"' + 'value="' + j + '"';
-				if (j == newForm.inputs.defaultchecked) {
-					htmlcode = htmlcode + ' checked="checked"/>';
-				} else {
-					htmlcode = htmlcode + '/>';
-				}
-				//add the label
-				htmlcode = htmlcode + '<label for="tp-rd-' + newForm.attr + '">' + newForm.inputs.value[j] + '</label><br />';
-			}					
+			htmlcode = htmlcode + radioInput(newForm.name, newForm.inputs );			
 			break;
+			
+		case "multi" :
+			htmlcode = htmlcode + multiInput(newForm.name, newForm.inputs );	
+			break;
+			
 		default:
 			break;
 	}
@@ -91,7 +191,7 @@ function tpButtonDisplay() {
 		document.getElementById('tp-bt-prev').style.display = 'inline-block';
 	}
 	
-	if (g_tpForm[g_tpCurFormNo].attr == "end") {
+	if (g_tpForm[g_tpCurFormNo].name == "end") {
 		//hide next button
 		document.getElementById('tp-bt-next').style.display = 'none';
 		//show submit button
@@ -138,7 +238,7 @@ function tpNext() {
 	var nextFormNo = g_tpCurFormNo + 1;
 	
 	if (newForm.inputs.type === "radio") {
-		var name = nameRadio(newForm.attr);
+		var name = nameAttr(newForm.name);
 		var radios = document.getElementsByName(name);
 		for (var i = 0, length = radios.length; i < length; i++) {
 			if (radios[i].checked) {
